4e318d987e84fbffa3689e8a58cfa10141e0a8ad
protonsk <proto@ngs.ru>
2022-11-01 17:29:25 +0300

    - New AccuWeather UI tests added - README edited

---
+++ src/main/java/pages/AWMainPage.java
@@ -0,0 +1,41 @@
+package pages;
+
+import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.SelenideElement;
+
+import static com.codeborne.selenide.Selenide.$x;
+
+public class AWMainPage {
+    public final static String BASE_URL = "https://www.accuweather.com/";
+
+    //private final SelenideElement pageHeader = $x("//span[@class = 'page_header_span' ]");
+    //data entry field
+    private final SelenideElement searchLocation = $x("//input[@class = 'search-input' ]");
+    //private final SelenideElement searchBtn = $x("//span[@text = 'Location' ]");
+
+    //Check the Title of a page
+    public String getHeaderText(){
+        //return pageHeader.text();
+        return Selenide.title();
+    }
+
+    //Enter the Search text and get new page
+    public AWCityPage SearchLocation(String location) throws InterruptedException {
+        searchLocation.sendKeys(location);
+        searchLocation.pressEnter();
+        Thread.sleep(2000);
+        //searchBtn.click();
+        return new AWCityPage();
+    }
+
+
+    public AWMainPage() throws InterruptedException {
+        openPage(BASE_URL);
+        Thread.sleep(5000);
+    }
+
+    public void openPage(String url){
+        Selenide.open(url);
+    }
+
+}
